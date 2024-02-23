d48adb14f0a5e1eb718ba4e2a49355fd673725df
Sunan13208 <sunanrabbi1918@gmail.com>
2024-02-23 15:18:21 +0600

    deploy

--- dist/app.js
+++ dist/app.js
@@ -9,9 +9,9 @@ const notFound_1 = __importDefault(require("./middleware/notFound"));
 const globalErrorHandle_1 = __importDefault(require("./middleware/globalErrorHandle"));
 const router_1 = __importDefault(require("./router"));
 const app = (0, express_1.default)();
-const allowedOrigins = ['http://localhost:5173', 'https://fullstack-2-8255d.web.app/', 'https://fullstack-2-8255d.firebaseapp.com/'];
+const allowedOrigins = ['http://localhost:5173', 'https://fullstack-2-8255d.web.app', 'https://fullstack-2-8255d.firebaseapp.com'];
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({ origin: "*", credentials: true }));
+app.use((0, cors_1.default)({ origin: "https://fullstack-2-8255d.web.app", credentials: true }));
 app.use('/api', router_1.default);
 app.get('/', (req, res) => {
     res.send("Welcome to Fullstack's Assignment 2 | Redux-2 ");
