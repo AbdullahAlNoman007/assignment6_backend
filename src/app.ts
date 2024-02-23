bedc33d2cc5b5b9753b0246e0b045f8f12a64705
Sunan13208 <sunanrabbi1918@gmail.com>
2024-02-23 15:17:23 +0600

    deploy

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,10 @@ import globalErrorHandle from './middleware/globalErrorHandle';
 import router from './router';
 
 const app: Application = express();
-const allowedOrigins = ['http://localhost:5173', 'https://fullstack-2-8255d.web.app/', 'https://fullstack-2-8255d.firebaseapp.com/'];
+const allowedOrigins = ['http://localhost:5173', 'https://fullstack-2-8255d.web.app', 'https://fullstack-2-8255d.firebaseapp.com'];
 
 app.use(express.json());
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: "https://fullstack-2-8255d.web.app", credentials: true }));
 
 app.use('/api', router)
 
